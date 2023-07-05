1e2c2de0b85eb16a858ea8d402d09016172ce78b
Facundo Fernandez <facu.fernandez1987@gmail.com>
2023-07-05 15:18:54 +0200

    Commit #3

--- src/main/java/org/project/managers/WebDriverManager.java
+++ src/main/java/org/project/managers/WebDriverManager.java
@@ -4,9 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.nio.file.Paths;
-import java.util.HashMap;
-
 public class WebDriverManager {
 
     private WebDriver driver;
@@ -21,10 +18,6 @@ public class WebDriverManager {
 
     private WebDriver createDriver() {
         ChromeOptions options = new ChromeOptions();
-        HashMap<String, Object> chromePref = new HashMap<>();
-        chromePref.put("download.default_directory", Paths.get("target/download").toAbsolutePath().toString());
-        chromePref.put("profile.content_settings.exceptions.automatic_downloads.*.setting", 1);
-        options.setExperimentalOption("prefs", chromePref);
         io.github.bonigarcia.wdm.WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
